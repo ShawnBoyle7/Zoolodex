72814bfd5cda933f9b55ce972850b065eb6b9238
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-21 17:37:51 -0600

    auth components

---
+++ react-app/src/components/auth/LoginForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { login } from '../../store/session';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector(state => state.session.user);
+
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(email, password));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
+  return (
+    <form onSubmit={onLogin}>
+      <div>
+        {errors.map((error, idx) => (
+          <div key={idx}>{error}</div>
+        ))}
+      </div>
+      <div>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={updateEmail}
+        />
+      </div>
+      <div>
+        <input
+          name='password'
+          type='password'
+          autoComplete="on"
+          placeholder='Password'
+          value={password}
+          onChange={updatePassword}
+        />
+        <button type='submit'>Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
