ee65e0691059a62942e73e8d78bc8155a6ec447b
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-21 16:51:53 -0600

    user models

---
+++ app/models/user.py
@@ -0,0 +1,36 @@
+from .db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    img_url = db.Column(db.String(255), nullable=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'username': self.username,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'imgUrl': self.img_url,
+        }
