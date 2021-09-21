a9f9e058e8296f50f76ba271ddcc72af7750bb69
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-21 17:42:56 -0600

    created basic nav links for auth

---
+++ react-app/src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from './auth/LogoutButton';
+
+const NavBar = () => {
+  return (
+    <nav>
+        <div>
+          <NavLink to='/' exact={true} activeClassName='active'>
+            Home
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to='/login' exact={true} activeClassName='active'>
+            Login
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+            Sign Up
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to='/users' exact={true} activeClassName='active'>
+            Users
+          </NavLink>
+        </div>
+        <div>
+          <LogoutButton />
+        </div>
+    </nav>
+  );
+}
+
+export default NavBar;
