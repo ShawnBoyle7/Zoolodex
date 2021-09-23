b08638ef110b047e4a933ee916b1ddfd3ee5b086
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-23 15:01:52 -0600

    suggestion form

--- app/forms/suggestion_form.py
+++ app/forms/suggestion_form.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
-from wtforms.validators import DataRequired, ValidationError
-from app.models import Suggestion
+from wtforms.validators import DataRequired
 
 class SuggestionForm(FlaskForm):
-    type = SelectField("type", choices=["animal", "region"])
+    type = SelectField("type", choices=["animal", "region"], validators=[DataRequired()])
+    title = StringField("title", validators=[DataRequired()])
+    description = StringField("description", validators=[DataRequired()])
+    img_url = StringField("img_url", validators=[DataRequired()])
+    user_id = IntegerField("user_id", validators=[DataRequired()])
