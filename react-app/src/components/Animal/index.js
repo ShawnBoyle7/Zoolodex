d2087f30feaaeabd4b33f760ab7f98a1f77012ce
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-22 23:44:32 -0600

    animals/animal page rendered

--- react-app/src/components/Animal/index.js
+++ react-app/src/components/Animal/index.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Animal = () => {
-    const animals = useSelector(state => state.animals)
-    console.log(animals)
+    
     return (
         <>
             
--- react-app/src/components/Animals/index.js
+++ react-app/src/components/Animals/index.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom"
 
 const Animals = () => {
-    
+    const animals = Object.values(useSelector(state => state.animals))
+
     return (
         <>
-            
+            <div className="animals-div">
+                {animals.map(animal => 
+                    <div className="animal-link" key={animal.id}>
+                        <Link to={`animals/${animal.id}`}>
+                            <div className="animal-name">
+                                {animal.subSpecies}
+                            </div>
+                        </Link>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
