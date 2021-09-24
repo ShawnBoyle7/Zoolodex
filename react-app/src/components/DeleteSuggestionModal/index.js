d4718a00a3c38b74498547d7509e62ac24be5326
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-24 11:00:34 -0600

    react portion of delete suggestion done

---
+++ react-app/src/components/DeleteSuggestionModal/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Modal } from '../../context/Modal';
+
+function DeleteSuggestionModal({ suggestionId, setShowModal, showModal }) {
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        dispatch(deleteSuggestion(suggestionId))
+        setShowModal(false)
+    }
+
+    return (
+        <>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <p>Are you sure?</p>
+                    <div>
+                        {/* Confirm Delete button which dispatches the suggestion ID to our redux store */}
+                        <button onClick={handleDelete}>Delete</button>
+                        {/* Cancel by setting show modal to false */}
+                        <button onClick={() => setShowModal(false)}>Cancel</button>
+                    </div>
+                </Modal>
+            )}
+        </>
+    )
+}
+
+export default DeleteSuggestionModal
