58170f0fb8619363bdbefa347e7edac81dd4d028
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-24 12:26:41 -0600

    fixed imagee rendering  on suggestions post, reformatted authorization intoo the suggestion tile and rendered  it on the proifle page as well

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,11 +3,11 @@ from app.models import db, User
 
 def seed_users():
     demo = User(
-        username='Demouser', email='demo@demo.io', first_name="demo", last_name="user", img_url="https://i.imgur.com/tGthvVP.png", password='password')
+        username='DemoUser', email='demo@demo.io', first_name="demo", last_name="user", img_url="https://i.imgur.com/tGthvVP.png", password='password')
     chadwick = User(
-        username='greek_life', email='chadwickrules@chadwick.io', first_name="Chadwick", last_name="Dezin", img_url="https://i.imgur.com/tGthvVP.png", password='password')
+        username='GreekLife', email='chadwickrules@chadwick.io', first_name="Chadwick", last_name="Dezin", img_url="https://i.imgur.com/tGthvVP.png", password='password')
     roman = User(
-        username='big_rome', email='the_rome@roman.io', first_name="Roman", last_name="Montera", img_url="https://i.imgur.com/tGthvVP.png", password='password')
+        username='BigRome', email='the_rome@roman.io', first_name="Roman", last_name="Montera", img_url="https://i.imgur.com/tGthvVP.png", password='password')
 
     db.session.add(demo)
     db.session.add(chadwick)
