77b184615258a4d288dec216672a1dfafccbad05
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-25 14:39:19 -0600

    redux store for edit  user

--- app/forms/edit_user_form.py
+++ app/forms/edit_user_form.py
@@ -3,9 +3,9 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
 class EditUserForm(FlaskForm):
-    user_id = IntegerField('user_id')
     email = StringField('email', validators=[DataRequired()])
     username = StringField('username', validators=[DataRequired()])
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
     password = StringField('password')
+    user_id = IntegerField('user_id')
