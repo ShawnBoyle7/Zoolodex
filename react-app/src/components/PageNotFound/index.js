3d86e6623a2a41f675f75fa9ed13eb46aadee774
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-25 16:33:42 -0600

    non-existent user profies now render a 404 component

---
+++ react-app/src/components/PageNotFound/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageNotFound = () => {
+    
+    return(
+        <h1>Page Not Found</h1>
+    )
+}
+
+export default PageNotFound;
--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -3,42 +3,51 @@ import { useSelector } from "react-redux";
 import SuggestionTile from "../SuggestionTile";
 import EditUserFormModal from "../EditUserFormModal";
 import { useParams } from "react-router-dom";
+import PageNotFound from "../PageNotFound";
 
 const Profile = () => {
     const { userId } = useParams();
 
     const sessionUser = useSelector(state => state.session?.user)
+    
+    // Finds the user of the profile from useParams for rendering their data
     const profileUser = Object.values(useSelector(state => state.users)).find(user => user.id === +userId)
-    
+
     const suggestions = Object.values(useSelector(state => state.suggestions)).filter(suggestion => suggestion?.userId === profileUser?.id)
-    
+
+    // Prevents non-existent profiles from rendering
+    const users = Object.values(useSelector(state => state.users))
+    const finalUserId = users?.length
+    const userExists = userId <= finalUserId
+
     return(
-        <>
-            
-            <div className="profile-username">
-                <h2>{profileUser?.username}</h2>
-            </div>
-
-            {profileUser?.id === sessionUser?.id && 
-                <div className="edit-user">
-                    <EditUserFormModal/>
+            userExists ?
+            <>
+                <div className="profile-username">
+                    <h2>{profileUser?.username}</h2>
                 </div>
-            }
-            
-            <div className="profile-image">
-                <img src={profileUser?.imgUrl} alt="profile"/>
-            </div>
 
-            <div className="profile-suggestions">
-                <h1>Suggestions</h1>
-                {suggestions?.map(suggestion => 
-                    <div className="profile-suggestion" key={suggestion.id}>
-                        <SuggestionTile suggestion={suggestion}/>
+                {profileUser?.id === sessionUser?.id && 
+                    <div className="edit-user">
+                        <EditUserFormModal/>
                     </div>
-                )}
-            </div>
+                }
+                
+                <div className="profile-image">
+                    <img src={profileUser?.imgUrl} alt="profile"/>
+                </div>
 
-        </>
+                <div className="profile-suggestions">
+                    <h1>Suggestions</h1>
+                    {suggestions?.map(suggestion => 
+                        <div className="profile-suggestion" key={suggestion.id}>
+                            <SuggestionTile suggestion={suggestion}/>
+                        </div>
+                    )}
+                </div>
+            </>
+            : 
+            <PageNotFound/>
     )
 };
 
