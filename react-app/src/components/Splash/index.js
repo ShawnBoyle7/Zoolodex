dc6d6439444c81a3a2e31d3d8b6d98357dd52204
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-27 11:29:40 -0600

    testing carousel library and background images

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React, { useRef } from "react";
+import { Rerousel } from 'rerousel';
+import { useSelector } from "react-redux";
+import "./Splash.css"
 
 const Splash = () => {
 
-    return(
-        <>
-            <h1>Splash</h1>
-        </>
-    )
+        return (
+            <div className="splash-background">
+                
+            </div>
+            
+        )
 };
 
 export default Splash;
+
+
+{/* <Rerousel itemRef={thing} interval={7000}>
+    {regions.map(region => 
+            <div className="carousel-image-div" ref={thing}>
+                <img className="carousel-image" src={region.imgUrl1} key={region.id}/>
+            </div>
+    )}
+</Rerousel> */}
