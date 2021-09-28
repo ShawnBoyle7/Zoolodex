8d1f5f71182a30e8d00a8088fcb67dca5cf9d0e4
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-28 15:11:35 -0600

    in progress of nav scrolling change, added scroll checker and debugging

--- react-app/src/components/NavigationBar/index..js
+++ react-app/src/components/NavigationBar/index..js
@@ -21,59 +21,72 @@ const NavigationBar = ({ authenticated }) => {
     history.push("/")
 }
 
+const scrollChecker = () => {
+    const root = document.querySelector("#root")
+
+    root.scrollTop === 0 
+    ?
+    root.classList.add("navigation-default")
+    :
+    root.classList.add("navigation-scrolled")
+}
+
 return (
-    <nav>
-        <div className="nav-links-div">
-            <NavLink className="logo" to='/' exact={true} activeClassName='active'>
-                Zoolodex
-            </NavLink>
+    <>
+        {scrollChecker()}
+        <nav>
+            <div className="nav-links-div">
+                <NavLink className="logo" to='/' exact={true} activeClassName='active'>
+                    Zoolodex
+                </NavLink>
 
-            <NavLink to='/animals' exact={true} activeClassName='active'>
-                Animals
-            </NavLink>
+                <NavLink to='/animals' exact={true} activeClassName='active'>
+                    Animals
+                </NavLink>
 
-            <NavLink to='/regions' exact={true} activeClassName='active'>
-                Regions
-            </NavLink>
+                <NavLink to='/regions' exact={true} activeClassName='active'>
+                    Regions
+                </NavLink>
 
-            <NavLink to='/suggestions' exact={true} activeClassName='active'>
-                Suggestions
-            </NavLink>
-        </div>
-
-        <div className="search-div">
-                <form className="search-form">
-                    <i className="fas fa-search"></i>
-                    <input 
-                    placeholder='Explore Your Curiosity'
-                    />
-                </form>
+                <NavLink to='/suggestions' exact={true} activeClassName='active'>
+                    Suggestions
+                </NavLink>
             </div>
 
+            <div className="search-div">
+                    <form className="search-form">
+                        <i className="fas fa-search"></i>
+                        <input 
+                        placeholder='Explore Your Curiosity'
+                        />
+                    </form>
+                </div>
 
-        <div className="navigation-auth">
-            {!authenticated ?
-            <>
-                <AboutModal/>
-                <LoginFormModal/>
-                <SignUpFormModal/>
-                <button onClick={loginDemo}>Demo</button>
-            </>
-            :
-            <>
-                <span className="welcome-message">
-                    Welcome, {sessionUser.firstName}
-                </span>
-                <NavLink to={`/users/${sessionUser.id}`} exact={true} activeClassName='active'>
-                    Profile
-                </NavLink>
-                <AboutModal/>
-                <SuggestionFormModal/>
-                <LogoutButton/>
-            </>
-            }
-        </div>
-    </nav>
+
+            <div className="navigation-auth">
+                {!authenticated ?
+                <>
+                    <AboutModal/>
+                    <LoginFormModal/>
+                    <SignUpFormModal/>
+                    <button onClick={loginDemo}>Demo</button>
+                </>
+                :
+                <>
+                    <span className="welcome-message">
+                        Welcome, {sessionUser.firstName}
+                    </span>
+                    <NavLink to={`/users/${sessionUser.id}`} exact={true} activeClassName='active'>
+                        Profile
+                    </NavLink>
+                    <AboutModal/>
+                    <SuggestionFormModal/>
+                    <LogoutButton/>
+                </>
+                }
+            </div>
+        </nav>
+    </>
     );
 }
 
