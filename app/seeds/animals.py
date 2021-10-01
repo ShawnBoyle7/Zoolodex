996000ec9fd0acf968e357cefe2ffa8838fa3909
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-01 13:01:43 -0600

    all regions reformatted into one image

--- app/seeds/animals.py
+++ app/seeds/animals.py
@@ -15,21 +15,22 @@ def seed_animals():
 
     animals = []
     
-    animals.append(Animal(group="Mammal", family="Felidae", species="Leopard", sub_species="Amur Leopard", img_url="https://i.imgur.com/GELNoJV.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
+    animals.append(Animal(group="Reptile", family="Testudinidae", species="Galapagos Tortoise", sub_species="Santiago Island Giant Tortoise", img_url="https://i.imgur.com/UAY0kHR.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
+
+    animals.append(Animal(group="Mammal", family="Felidae", species="Lion", sub_species="Katanga Lion", img_url="https://i.imgur.com/n3SF630.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
+
+    animals.append(Animal(group="Reptile", family="Boidae", species="Anaconda", sub_species="Green Anaconda", img_url="https://i.imgur.com/rWTiPPD.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
+
+    animals.append(Animal(group="Mammal", family="Felidae", species="Leopard", sub_species="Amur Leopard", img_url="https://i.imgur.com/qzCntDT.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
     animals.append(Animal(group="Mammal", family="Canidae", species="Wolf", sub_species="Arabian Wolf", img_url="https://i.imgur.com/mImcDDt.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
     animals.append(Animal(group="Mammal", family="Felidae", species="Tiger", sub_species="Amur Tiger", img_url="https://i.imgur.com/G9PPFEj.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
-    animals.append(Animal(group="Mammal", family="Felidae", species="Lion", sub_species="Katanga Lion", img_url="https://i.imgur.com/n3SF630.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
-
     animals.append(Animal(group="Aves", family="Accipitridae", species="Red-tailed Hawk", sub_species="Western Red-tailed Hawk", img_url="https://i.imgur.com/Q4Q4GYK.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
     animals.append(Animal(group="Aves", family="Strigidae", species="Spotted Owl", sub_species="Northern Spotted Owl", img_url="https://i.imgur.com/qzdWzN9.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
-    animals.append(Animal(group="Reptile", family="Testudinidae", species="Galapagos Tortoise", sub_species="Santiago Island Giant Tortoise", img_url="https://i.imgur.com/UAY0kHR.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
-
-    animals.append(Animal(group="Reptile", family="Boidae", species="Anaconda", sub_species="Green Anaconda", img_url="https://i.imgur.com/rWTiPPD.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
     animals.append(Animal(group="Amphibian", family="Ambystomatidae", species="Long-toed Salamander", sub_species="Santa Cruz Long-toed Salamander", img_url="https://i.imgur.com/6fY4nuc.jpg", origins=faker.paragraph(nb_sentences=180), traits=faker.paragraph(nb_sentences=180), ecosystem_influence=faker.paragraph(nb_sentences=180)))
 
