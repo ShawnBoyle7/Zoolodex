8f51914a0303375f620aba707961ba1f09c24645
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-27 13:53:11 -0600

    search dropdown preliminary styling

--- react-app/src/components/SearchDropdown/index.js
+++ react-app/src/components/SearchDropdown/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "./SearchDropdown.css"
 
 const SearchDropdown = ({ searchQuery, setSearchQuery, setRenderSearchDropdown  }) => {
     const animals = Object.values(useSelector(state => state.animals))
